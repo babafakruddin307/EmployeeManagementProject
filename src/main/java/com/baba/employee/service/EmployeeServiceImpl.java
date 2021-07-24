17e8bafeb8011ba4a52ea8c5eddcfad846746db9
Baba Fakruddin <babafakruddin307@gmail.com>
2021-07-24 16:15:25 +0530

    added pagination and sorting mehthods

--- src/main/java/com/baba/employee/service/EmployeeServiceImpl.java
+++ src/main/java/com/baba/employee/service/EmployeeServiceImpl.java
@@ -1,9 +1,13 @@
 package com.baba.employee.service;
-
+import com.baba.employee.model.*;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.baba.employee.model.Employee;
@@ -42,4 +46,12 @@ public class EmployeeServiceImpl implements EmployeeService {
 		this.repository.deleteById(id);
 	}
 
+	@Override
+	public Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
+		Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+			Sort.by(sortField).descending();
+		
+		Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
+		return this.repository.findAll(pageable);
+	}
 }
